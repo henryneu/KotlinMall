74655d9f8c96e339f9ae80469efe12b117bd6421
周振华 <zhenhua.zhou@BKJK.COM>
2018-06-19 11:17:44 +0800

    多模块框架搭建

---
+++ UserCenter/src/main/java/com/bkjk/kotlin/usercenter/ui/activity/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.bkjk.kotlin.usercenter.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.bkjk.kotlin.usercenter.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
