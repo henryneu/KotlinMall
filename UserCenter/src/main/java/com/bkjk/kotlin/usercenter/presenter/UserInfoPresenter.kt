21b651c082872a05ba6c1481084ddead8b2d4ceb
周振华 <zhenhua.zhou@BKJK.COM>
2018-07-14 23:57:28 +0800

    完善用户信息界面，添加资源文件

--- UserCenter/src/main/java/com/bkjk/kotlin/usercenter/presenter/UserInfoPresenter.kt
+++ UserCenter/src/main/java/com/bkjk/kotlin/usercenter/presenter/UserInfoPresenter.kt
@@ -1,9 +1,30 @@
 package com.bkjk.kotlin.usercenter.presenter
 
+import com.bkjk.kotlin.baselibrary.ext.execute
 import com.bkjk.kotlin.baselibrary.presenter.BasePresenter
+import com.bkjk.kotlin.baselibrary.rx.BaseObserver
 import com.bkjk.kotlin.usercenter.presenter.view.UserInfoView
+import com.bkjk.kotlin.usercenter.service.UploadService
+import com.bkjk.kotlin.usercenter.service.UserService
 import javax.inject.Inject
 
 class UserInfoPresenter @Inject constructor(): BasePresenter<UserInfoView>() {
 
+    @Inject
+    lateinit var userService: UserService
+
+    @Inject
+    lateinit var uploadService: UploadService
+
+    fun getUploadToken() {
+        if (!checkNetWork())
+            return
+
+        mView.showLoading()
+        uploadService.getUploadToken().execute(object : BaseObserver<String>(mView) {
+            override fun onNext(t: String) {
+                mView.onGetUploadTokenResult(t)
+            }
+        }, lifecycleProvider)
+    }
 }
